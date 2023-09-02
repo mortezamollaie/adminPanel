7953e0a94b8f7c328ce71e39b63dec73d378eb11
morteza <mm.gov.1381@gmail.com>
2023-09-02 15:15:40 +0330

    show or hide header

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -15,4 +15,17 @@ $(document).ready(function () {
       $("#sidebar-toggle-show").removeClass("d-none");
     }, 300);
   });
+
+  $("#sidebar-toggle-show").click(function () {
+    $("#sidebar").fadeIn(300);
+    setTimeout(function () {
+      removeAllSidebarToggleClass();
+      $("#sidebar-toggle-hide").removeClass("d-none");
+      $("#sidebar-toggle-show").addClass("d-none");
+    }, 300);
+  });
+
+  $("#menu-toggle").click(function () {
+    $("#body-header").toggle(300);
+  });
 });
